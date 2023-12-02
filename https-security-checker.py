827eb470ca6f8aa356cd70ec3efe76440a1cab0c
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-12-02 18:59:06 +0100

    Update https-security-checker.py

--- https-security-checker.py
+++ https-security-checker.py
@@ -3,10 +3,8 @@ from bs4 import BeautifulSoup
 import ssl
 
 def check_https_security(url):
-    # Ignorowanie błędów certyfikatu SSL (tylko do celów testowych)
     ssl._create_default_https_context = ssl._create_unverified_context
 
-    # Pobranie strony internetowej
     try:
         response = requests.get(url, timeout=5)
         response.raise_for_status()
@@ -14,11 +12,9 @@ def check_https_security(url):
         print(f"[-] Unable to retrieve the website: {e}")
         return
 
-    # Sprawdzenie certyfikatu SSL
     if not response.history and response.url.startswith("https"):
         print("[+] Website is using HTTPS")
 
-        # Analiza HTML w celu znalezienia tagów meta związanych z bezpieczeństwem
         soup = BeautifulSoup(response.text, 'html.parser')
         meta_tags = soup.find_all('meta')
 
@@ -34,8 +30,22 @@ def check_https_security(url):
         else:
             print("[-] No security information found")
 
+        # Sprawdzanie wycieków DNS/IP
+        check_dns_ip_leaks(url)
+
+        # Sprawdzanie wycieków baz danych
+        check_database_leaks(url)
+
     else:
         print("[-] Website is not using HTTPS")
+
+def check_dns_ip_leaks(url):
+    # Tutaj możesz umieścić kod do sprawdzania wycieków DNS/IP
+    pass
+
+def check_database_leaks(url):
+    # Tutaj możesz umieścić kod do sprawdzania wycieków baz danych
+    pass
 
 if __name__ == "__main__":
     url = input("Enter the website URL: ")
