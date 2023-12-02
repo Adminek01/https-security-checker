e89fd109b08a5298dd8b9aa58f1209ffd8e6bf27
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2023-12-02 19:46:20 +0100

    Update https-security-checker.py

--- https-security-checker.py
+++ https-security-checker.py
@@ -1,68 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
-import ssl
+import re
 from colorama import Fore, Style
 
-def check_website_security(url):
-    ssl._create_default_https_context = ssl._create_unverified_context
-
-    try:
-        response = requests.get(url, timeout=5)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(f"[-] Unable to retrieve the website: {e}")
-        return
-
-    if not response.history and response.url.startswith("https"):
-        print("[+] Website is using HTTPS")
-        print_detailed_info(response)
-
-        soup = BeautifulSoup(response.text, 'html.parser')
-        meta_tags = soup.find_all('meta')
-
-        security_info = []
-        for tag in meta_tags:
-            if 'security' in tag.get('name', '').lower() or 'security' in tag.get('content', '').lower():
-                security_info.append(tag)
-
-        if security_info:
-            print("[+] Security information found:")
-            for tag in security_info:
-                print(f"    - {tag}")
-        else:
-            print("[-] No security information found")
-
-        # Sprawdzanie wycieków DNS/IP
-        check_dns_ip_leaks(url)
-
-        # Sprawdzanie wycieków baz danych
-        check_database_leaks(url)
-
-        # Sprawdzanie ataków brute force
-        check_brute_force_attack(url)
-
-        # Sprawdzanie podatności na SQL injection
-        check_sql_injection_vulnerability(url)
-
+def analyze_url(url):
+    # Sprawdzenie, czy URL zawiera potencjalnie niebezpieczne znaki
+    dangerous_characters = re.findall(r'[\'"<>;=&]', url)
+    if dangerous_characters:
+        print(Fore.RED + "[-] Potentially dangerous characters found in the URL." + Style.RESET_ALL)
     else:
-        print("[-] Website is not using HTTPS")
+        print(Fore.GREEN + "[+] URL seems safe from dangerous characters." + Style.RESET_ALL)
 
-def print_detailed_info(response):
-    print("[+] Detailed Information:")
-    print(f"    - Server: {response.headers.get('server', 'N/A')}")
-    print(f"    - SSL Certificate: {response.headers.get('x-powered-by', 'N/A')}")
-    print(f"    - Content Type: {response.headers.get('content-type', 'N/A')}")
+def analyze_forms(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-def check_dns_ip_leaks(url):
-    # Tutaj możesz umieścić kod do sprawdzania wycieków DNS/IP
-    pass
+        forms = soup.find_all('form')
+        if forms:
+            print(Fore.GREEN + "[+] Forms found on the page." + Style.RESET_ALL)
+            # Tutaj możesz dodać kod analizujący pola formularza
+        else:
+            print(Fore.YELLOW + "[-] No forms found on the page." + Style.RESET_ALL)
+
+    except requests.exceptions.RequestException as e:
+        print(f"[-] Unable to analyze forms: {e}")
+
+def check_sql_injection_vulnerability(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        if re.search(r'\b(UNION|OR|AND)\b', response.text, re.IGNORECASE):
+            print(Fore.RED + "[-] Possible SQL Injection Vulnerability detected." + Style.RESET_ALL)
+        else:
+            print(Fore.GREEN + "[+] No SQL Injection Vulnerability found." + Style.RESET_ALL)
+
+    except requests.exceptions.RequestException as e:
+        print(f"[-] Unable to check for SQL Injection Vulnerability: {e}")
 
 def check_database_leaks(url):
     # Tutaj możesz umieścić kod do sprawdzania wycieków baz danych
     pass
 
 def check_brute_force_attack(url):
-    # Sprawdzanie formularza logowania i podejrzenie ataku brute force
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -70,26 +52,27 @@ def check_brute_force_attack(url):
 
         login_form = soup.find('form', {'action': True, 'method': True})
         if login_form:
-            print("[+] Login form found. Checking for potential brute force attack.")
+            print(Fore.GREEN + "[+] Login form found." + Style.RESET_ALL)
             # Tutaj możesz dodać kod sprawdzający potencjalne ataki brute force
         else:
-            print("[+] No login form found on the page.")
+            print(Fore.YELLOW + "[-] No login form found on the page." + Style.RESET_ALL)
 
     except requests.exceptions.RequestException as e:
         print(f"[-] Unable to check for brute force attack: {e}")
 
-def check_sql_injection_vulnerability(url):
-    # Sprawdzanie podatności na SQL injection
-    try:
-        response = requests.get(url + "/?id=1' OR '1'='1'--")
-        if 'error' in response.text.lower() or 'exception' in response.text.lower():
-            print("[+] Possible SQL Injection Vulnerability detected.")
-        else:
-            print("[+] No SQL Injection Vulnerability found.")
-    except requests.exceptions.RequestException as e:
-        print(f"[-] Unable to check for SQL Injection Vulnerability: {e}")
+def check_ddos_attack(url):
+    # Tutaj możesz umieścić kod do sprawdzania ataków DDoS
+    pass
+
+def check_security(url):
+    analyze_url(url)
+    analyze_forms(url)
+    check_sql_injection_vulnerability(url)
+    check_database_leaks(url)
+    check_brute_force_attack(url)
+    check_ddos_attack(url)
 
 if __name__ == "__main__":
     print(Fore.BLUE + "[+] This program has been created for educational purposes and security testing only." + Style.RESET_ALL)
     url = input("Enter the website URL: ")
-    check_website_security(url)
+    check_security(url)
