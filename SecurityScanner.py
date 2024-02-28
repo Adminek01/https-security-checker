faed64e5c18317cbb56d7802cbb673059d733c50
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2024-02-28 10:05:51 +0100

    Update SecurityScanner.py

--- SecurityScanner.py
+++ SecurityScanner.py
@@ -26,10 +26,13 @@ try:
 except Exception:
     import http.cookiejar as cookielib
 
-DEBUG        = False
+DEBUG = False
 EMAIL_FORMAT = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 __version__ = "1.61"
+
+
+# TUTAJ DODAJ POZOSTAŁE FUNKCJE I KODY ZADANE W POPRZEDNIM KOMUNIKACIE
 
 def personal_data_scan(target_url):
     """
@@ -47,10 +50,10 @@ def personal_data_scan(target_url):
         # Użyj innego serwera DNS do wykonywania żądań
         dns_servers = ['1.1.1.1', '1.0.0.1']  # Inne serwery DNS
         dns_server = random.choice(dns_servers)
-        
+
         # Ustaw adres IP serwera DNS jako parametr dns
         response = httpx.get(target_url, dns=(dns_server, dns_server))
-        
+
         soup = BeautifulSoup(response.text, 'html.parser')
         found_data = {}
 
@@ -69,6 +72,7 @@ def personal_data_scan(target_url):
     except Exception as e:
         print(f"An error occurred during personal data scan: {e}")
 
+
 def import_submodules(package, recursive=True):
     """Get all the holehe submodules"""
     if isinstance(package, str):
@@ -81,20 +85,24 @@ def import_submodules(package, recursive=True):
             results.update(import_submodules(full_name))
     return results
 
-def get_functions(modules,args=None):
+
+def get_functions(modules, args=None):
     """Transform the modules objects to functions"""
     websites = []
 
     for module in modules:
-        if len(module.split(".")) > 3 :
+        if len(module.split(".")) > 3:
             modu = modules[module]
             site = module.split(".")[-1]
-            if args is not None and args.nopasswordrecovery==True:
-                if  "adobe" not in str(modu.__dict__[site]) and "mail_ru" not in str(modu.__dict__[site]) and "odnoklassniki" not in str(modu.__dict__[site]) and "samsung" not in str(modu.__dict__[site]):
+            if args is not None and args.nopasswordrecovery == True:
+                if "adobe" not in str(modu.__dict__[site]) and "mail_ru" not in str(
+                        modu.__dict__[site]) and "odnoklassniki" not in str(modu.__dict__[site]) and "samsung" not in str(
+                        modu.__dict__[site]):
                     websites.append(modu.__dict__[site])
             else:
                 websites.append(modu.__dict__[site])
     return websites
+
 
 def check_update():
     """Check and update holehe if not the last version"""
@@ -119,8 +127,106 @@ def check_update():
         print("Holehe has just been updated, you can restart it.")
         exit()
 
+
 def credit():
     """Print Credit"""
     print('Twitter : @palenath')
     print('Github : https://github.com/megadose/holehe')
-    print('For BTC Donations : 1FHDM
+    print('For BTC Donations : 1FHDM')
+#!/usr/bin/python3
+
+import argparse
+import csv
+import math
+import re
+import time
+from datetime import datetime
+from functools import reduce
+from random import choice
+from multiprocessing import Pool, cpu_count, current_process, freeze_support
+from tqdm import tqdm
+from bs4 import BeautifulSoup
+from termcolor import colored
+import httpx
+import trio
+from subprocess import Popen, PIPE
+import os
+from argparse import ArgumentParser
+import csv
+from datetime import datetime
+import time
+import importlib
+import pkgutil
+import hashlib
+import re
+import sys
+import string
+import random
+import json
+from holehe.localuseragent import ua
+from holehe.instruments import TrioProgress
+
+ENGINES = {
+    "ahmia": "http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion",  # Offline?
+    "darksearchio": "http://darksearch.io",
+    "onionland": "http://3bbad7fauom4d6sgppalyqddsqbf5u5p56b5k5uk2zxsy3d6ey2jobad.onion",
+    "notevil": "http://hss3uro2hsxfogfq.onion",  # Offline?
+    "darksearchenginer": "http://l4rsciqnpzdndt2llgjx3luvnxip7vbyj6k6nmdy4xs77tx6gkd24ead.onion",
+    "phobos": "http://phobosxilamwcg75xt22id7aywkzol6q6rfl2flipcqoc4e4ahima5id.onion",
+    "onionsearchserver": "http://3fzh7yuupdfyjhwt3ugzqqof6ulbcl27ecev33knxe3u7goi3vfn2qqd.onion",
+    "torgle": "http://no6m4wzdexe3auiupv2zwif7rm6qwxcyhslkcnzisxgeiw6pvjsgafad.onion",  # "torgle" -> "Submarine"
+    "torgle1": "http://torgle5fj664v7pf.onion",  # Offline?
+    "onionsearchengine": "http://onionf4j3fwqpeo5.onion",  # Offline?
+    "tordex": "http://tordex7iie7z2wcg.onion",  # Offline?
+    "tor66": "http://tor66sewebgixwhcqfnp5inzp5x5uohhdy3kvtnyfxc2e5mxiuh34iid.onion",
+    "tormax": "http://tormaxunodsbvtgo.onion",  # Offline?
+    "haystack": "http://haystak5njsmn2hqkewecpaxetahtwhsbsa64jom2k22z5afxhnpxfid.onion",
+    "multivac": "http://multivacigqzqqon.onion",  # Offline?
+    "evosearch": "http://evo7no6twwwrm63c.onion",  # Offline?
+    "deeplink": "http://deeplinkdeatbml7.onion",  # Offline?
+}
+
+desktop_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',  # Tor Browser for Windows and Linux
+    'Mozilla/5.0 (Android 10; Mobile; rv:91.0) Gecko/91.0 Firefox/91.0',  # Tor Browser for Android
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) '
+    'AppleWebKit/602.2.14 (KHTML, like Gecko) Version/10.0.1 Safari/602.2.14',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) '
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) '
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
+]
+
+supported_engines = ENGINES
+
+available_csv_fields = [
+    "engine",
+    "name",
+    "link",
+    "domain"
+]
+
+def print_epilog():
+    epilog = "Available CSV fields: \n\t"
+    for f in available_csv_fields:
+        epilog += " {}".format(f)
+    epilog += "\n"
+    epilog += "Supported engines: \n\t"
+    for e in supported_engines.keys():
+        epilog += " {}".format(e)
+    return epilog
+
+parser = argparse.ArgumentParser(epilog=print_epilog(), formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("--proxy", default='localhost:9050', type=str, help="Set Tor proxy (default: 127.0.0.1:9050)")
+parser.add_argument("--output", default='output_$SEARCH_$DATE.txt', type=str,
+                   
+
+
